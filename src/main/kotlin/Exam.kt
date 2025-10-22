49740bacd629e274abca8639d3f55a40a09e67b4
mad.lrmk.ru <admin@lrmk.ru>
2025-10-22 13:00:29 +0300

    module 6 ready to work

--- src/main/kotlin/Exam.kt
+++ src/main/kotlin/Exam.kt
@@ -1,11 +1,43 @@
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.*
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.serialization.gson.gson
+import kotlinx.coroutines.launch
+import org.apache.poi.xwpf.usermodel.XWPFDocument
+import java.io.FileInputStream
+import java.io.FileOutputStream
 
 @Composable
 fun Exam() {
+    // Пишите здесь основной код для выполнения вашего задания
+
+}
+
+
+//** Средства получения данных **//
+// Клиент сетевого сервиса
+val client = {}
+// Адрес страницы для получения данных
+const val URL = ""
+// Класс данных для получения информации
+class Data
+// Функция для получения искомого значения
+suspend fun data() = ""
+
+
+//** Средства проверки строки на правильность **//
+private val regex = "".toRegex()
+private fun check(s: String) = "не успешно"
+
+
+//** Средства записи результатов в файл **//
+fun write(data: String, result: String) {
+    val file = "ТестКейс.docx"
 
 }
